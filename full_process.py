6afec56aaef7931ab7e48e3889615a8969584925
SergeyANOKHIN <anokhin.sergey@gmail.com>
2019-02-15 08:46:21 +0100

    work on full_process

--- full_process.py
+++ full_process.py
@@ -1,10 +1,9 @@
 '''
 0. New Mail Detected: sensor en Home Assisatant invoke run process
-1. Download Mail From GMail
-2. Save to local temp directory
-3. Analyse shots
-4. Copy to archive
-5. Send data to Elasticsearch
+1. Download Mail From GMail. Save to local temp directory
+2. Analyse shots
+3. Copy to archive
+4. Send data to Elasticsearch
 '''
 from Common.GmailContext import GmailContext
 from OpenCV.ThreeShots import ThreeShots
@@ -13,11 +12,15 @@ from OpenCV.YoloContext import YoloContext
 temp = 'temp'
 imap_folder = 'camera/foscam'
 
+### 1. Download Mail From GMail
 gmail = GmailContext()
 gmail.DownoadLastAttachments(imap_folder, temp)
 
+### 2. Analyse shots
 yolo = YoloContext('..\\camera-OpenCV-data\\weights\\yolov3-tiny')
-
-shots = ThreeShots.FromDir(temp)
+shots = ThreeShots.FromDir(None, temp)
 shots.yoloContext = yolo
 shots.Process(temp)
+
+### 3. Copy to archive
+### 4. Send data to Elasticsearch
