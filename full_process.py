1be2981d6551542cae9cb84696298155565a0866
SergeyANOKHIN <anokhin.sergey@gmail.com>
2019-02-27 12:30:27 +0100

    send new version

--- full_process.py
+++ full_process.py
@@ -6,37 +6,38 @@
 4. Copy to archive & Send data to Elasticsearch
 '''
 import os
+import shutil
 from Common.GmailContext import GmailContext
 from Common.ImapGmailHelper import ImapGmailHelper
 from OpenCV.ThreeShots import ThreeShots
 from OpenCV.YoloContext import YoloContext
 
-#temp = 'temp'
-temp = '../camera-OpenCV-data/Camera/Foscam/Day_Lilia_simple'
+temp = 'temp'
+shutil.rmtree(temp)
+#os.removedirs(temp)
+#temp = '../camera-OpenCV-data/Camera/Foscam/Day_Lilia_simple'
+
 imap_folder = 'camera/foscam'
 camera = 'Foscam'
-#os.removedirs(temp)
 
 ### 1. Download Mail From GMail
-# gmail = GmailContext()
-# gmail.DownoadLastAttachments(imap_folder, temp)
+gmail = GmailContext()
+gmail.DownoadLastAttachments(imap_folder, temp)
 
 ### 2. Analyse shots
-yolo = YoloContext('..\\camera-OpenCV-data\\weights\\yolov3-tiny')
-#yolo = YoloContext('..\\camera-OpenCV-data\\weights\\yolo-coco')
+#yolo = YoloContext('..\\camera-OpenCV-data\\weights\\yolov3-tiny')
+yolo = YoloContext('..\\camera-OpenCV-data\\weights\\yolo-coco')
 shots = ThreeShots.FromDir(None, temp)
 shots.yoloContext = yolo
 analyseData = shots.Analyse()
-#shots.Save(os.path.join(temp, 'MDAlarm_{:%Y%m%d-%H%M%S}-cts.jpg'))
-shots.Show()
+shots.Save(os.path.join(temp, 'MDAlarm_{:%Y%m%d-%H%M%S}-cts.jpg'))
+#shots.Show()
 
 ### 3. Send mail with analyse and log
-# imap = ImapGmailHelper()
+imap = ImapGmailHelper()
 mail_subject = analyseData.GetMailSubject(camera, shots.shot1.datetime)
 mail_body = analyseData.GetMailBody()
-print(mail_subject)
-print(mail_body)
-# imap.send_mail(mail_subject, "(log must be here)", [shots.output_filename])
+imap.send_mail(mail_subject, mail_body, [shots.output_filename])
 
 ### 4. Copy to archive & Send data to Elasticsearch
 # filesToArchive = shots.GetFullNamesArray()
