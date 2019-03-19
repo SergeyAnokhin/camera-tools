41df4d07c725ac8a5c2a85a049b09d12226e35d1
SergeyANOKHIN <anokhin.sergey@gmail.com>
2019-03-19 08:41:20 +0100

    yolo draw and results

--- Processors/YoloObjDetectionProcessor.py
+++ Processors/YoloObjDetectionProcessor.py
@@ -4,6 +4,14 @@ from Pipeline.Model.CamShot import CamShot
 from Pipeline.Model.ProcessingResult import ProcessingResult
 
 
+class YoloResult:
+    pass
+
+
+class YoloContext:
+    pass
+
+
 class YoloDetection:
 
     def __init__(self, detection, shot: CamShot):
@@ -56,15 +64,50 @@ class YoloCamShot:
         detections = [YoloDetection(d, self.shot) for d in itertools.chain(*layerOutputs)]
         self.detections = [d for d in detections if d.GetConfidence() > minConfidence]
 
-        boxes = [d.GetBoxCoordinates() for d in self.detections]
-        confidences = [d.GetConfidence() for d in self.detections]
-        self.idxs = cv2.dnn.NMSBoxes(boxes, confidences, minConfidence, threshold)
+        self.boxes = [d.GetBoxCoordinates() for d in self.detections]
+        self.confidences = [d.GetConfidence() for d in self.detections]
+        self.idxs = cv2.dnn.NMSBoxes(self.boxes, self.confidences, minConfidence, threshold)
 
-    def Draw(self):
-        pass
+    def Draw(self, colors, labels):
+        if len(self.idxs) == 0:
+            return
+        classIDs = [d.GetClassId() for d in self.detections]
 
-    def GetProcessResult(self):
-        pass
+        # loop over the indexes we are keeping
+        for i in self.idxs.flatten():
+            (x, y) = (self.boxes[i][0], self.boxes[i][1])
+            (w, h) = (self.boxes[i][2], self.boxes[i][3])
+            color = [int(c) for c in colors[classIDs[i]]]
+            text = "{}: {:.1f}".format(labels[classIDs[i]], self.confidences[i])
+
+            cv2.rectangle(self.shot.image, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(self.shot.image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, color, 2)
+
+    def GetProcessResult(self, labels):
+        # ensure at least one detection exists
+        if len(self.idxs) == 0:
+            return
+
+        results = []
+        classIDs = [d.GetClassId() for d in self.detections]
+        for i in self.idxs.flatten():
+            result = {}
+            (x, y) = (self.boxes[i][0], self.boxes[i][1])
+            (w, h) = (self.boxes[i][2], self.boxes[i][3])
+
+            (center_x, center_y) = (x + w//2,y + h//2)
+            
+            result['area'] = w * h
+            result['profile_proportion'] = round(h / w, 2)
+            result['center_coordinate'] = [center_x, center_y]
+            result['confidence'] = round(self.confidences[i], 2)
+            result['label'] = labels[classIDs[i]]
+            self.log.info(obj)
+
+            shot.imageAnalyseResult.objects.append(obj)
+
+        return results
     
 class YoloObjDetectionProcessor:
     confidence = 0.4
@@ -81,12 +124,12 @@ class YoloObjDetectionProcessor:
         if self.isPreLoaded:
             return
 
-        labelsPath = os.path.sep.join([yoloPath, "names.txt"])
+        labelsPath = os.path.sep.join([self.yoloPath, "names.txt"])
         self.LABELS = open(labelsPath).read().strip().split("\n")
         np.random.seed(42)
         self.COLORS = np.random.randint(0, 255, size=(len(self.LABELS), 3), dtype="uint8")
-        self.weightsPath = os.path.sep.join([yoloPath, "model.weights"])
-        self.configPath = os.path.sep.join([yoloPath, "model.cfg"])
+        self.weightsPath = os.path.sep.join([self.yoloPath, "model.weights"])
+        self.configPath = os.path.sep.join([self.yoloPath, "model.cfg"])
 
         self.log.debug("[LOAD] loading weights from disk... : " + self.weightsPath)
         start = time.time()
